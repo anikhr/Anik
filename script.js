e26241a99a7c9432b8adbe5e51842855f26b79a5
anikhr <hdas458@gmail.com>
2025-04-26 03:02:10 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple form validation
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    alert('Thank you for your message!');
+    event.preventDefault();
+});
